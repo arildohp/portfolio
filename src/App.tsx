e8b3879a6b6d40dd9069df0df58d169448956572
arildo <arildohp@gmail.com>
2024-02-17 14:40:26 -0300

    ATUALIZANDO PROJETO

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Teste from './Teste'
 import EstiloGlobal from './styles'
+import Sidebar from './containers/Sidebar'
 
 function App() {
   return (
     <>
       <EstiloGlobal />
-      <Teste />
+      <Sidebar></Sidebar>
     </>
   )
 }
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/arildohp" />
+    <Titulo>Arildo Horacio Pereira</Titulo>
+  </aside>
+)
+
+export default Sidebar
