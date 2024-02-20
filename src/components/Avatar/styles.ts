537bbc4a358b918699cab33a035767663c6ac41b
arildo <arildohp@gmail.com>
2024-02-20 09:37:03 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const FotoPerfil = styled.img`
+  max-width: 100%;
+  border-radius: 50%;
+  magin-bottom: 24px;
+`
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/arildohp" />
-    <Titulo fontSize={20}>Arildo Horacio Pereira</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Arildo </Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        arildohp
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema> Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
